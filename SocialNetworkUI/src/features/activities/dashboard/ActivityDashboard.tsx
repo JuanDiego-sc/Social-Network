35cb9c51b8c330f1843f41652dcbafd605a966fa
Juan Diego <juandsilva028@gmail.com>
2025-05-02 01:18:55 -0500

    CRUD logic without consuming the Activity API

---
+++ SocialNetworkUI/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,60 @@
+import { Grid} from '@mui/material';
+import { Activity } from '../../../lib/types';
+import ActivityList from './ActivityList';
+import ActivityDetail from '../details/ActivityDetail';
+import ActivityForm from '../form/ActivityForm';
+
+type Props = {
+    activities: Activity[]
+    selectActivity: (id: string) => void
+    cancelSelectActivity: () => void
+    selectedActivity?: Activity; 
+    editMode: boolean
+    openForm: (id: string) => void
+    closeForm: () => void
+    submitForm: (activity: Activity) => void
+    deleteActivity: (id: string) => void
+}
+
+export default function ActivityDashboard({
+    activities, 
+    selectActivity, 
+    cancelSelectActivity, 
+    selectedActivity,
+    editMode,
+    openForm,
+    closeForm,
+    submitForm,
+    deleteActivity
+        } : Props) 
+    {
+    return (
+        <>
+            <Grid container spacing={3}>
+                <Grid size={7}>
+                    <ActivityList 
+                    activities={activities}  
+                    selectActivity={selectActivity}
+                    deleteActivity={deleteActivity}
+                    />
+                </Grid>
+                <Grid size={5}>
+                    {selectedActivity && !editMode &&
+                    <ActivityDetail 
+                    activity={selectedActivity}
+                    cancelSelectActivity={cancelSelectActivity}
+                    openForm={openForm}>
+                    </ActivityDetail>
+                    }
+                    {editMode &&  
+                    <ActivityForm 
+                    closeForm={closeForm} 
+                    activity={selectedActivity}
+                    submitForm={submitForm}
+                    >
+                    </ActivityForm>}
+                </Grid>
+            </Grid>
+        </>
+    )
+}
